54f174acd3a4e98ed5782539782e8aad0ace8265
2018DELL-06 <git add .>
2024-12-20 14:32:29 +0900

    Bean&Dao修正

--- Suicabook/src/main/java/bean/EvaluationBean.java
+++ Suicabook/src/main/java/bean/EvaluationBean.java
@@ -3,37 +3,23 @@ package bean;
 import java.io.Serializable;
 import java.util.Date;
 
-public class EvaluationBean {
-	private int evaluation_id;
-	private int user_id;
-	private String book_id;
+public class EvaluationBean implements Serializable{
+	private int id;
 	private int evaluation_score;
 	private String evaluation_review;
-	private Date created_at;
-	private Date updated_at;
+	private int book_id;
+	private String img;
+	private String title;
 
-	public int getFavorite_id() {
-		return favorite_id;
+
+	public int getId() {
+		return id;
 	}
-	public void setFavorite_id(int favorite_id) {
-		this.favorite_id = favorite_id;
+
+	public void setId(int id) {
+		this.id = id;
 	}
-	public int getUser_id() {
-		return user_id;
-	}
-	
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
-	}
-	public String getBook_id() {
-		return book_id;
-	}
-	public void setBook_id(String book_id) {
-		this.book_id = book_id;
-	}
-	public Date getCreated_at() {
-		return created_at;
-	}	
+		
 	public void setEvaluation_score(int evaluation_score) {
 		this.evaluation_score = evaluation_score;
 	}
@@ -46,16 +32,23 @@ public class EvaluationBean {
 	public Date getEvaluation_review() {
 		return evaluation_review;
 	}
-	public void setCreated_at(Date created_at) {
-		this.created_at = created_at;
+	public void setBook_idt(int book_id) {
+		this.book_id = book_id;
 	}
-	public Date getUpdated_at() {
-		return updated_at;
+	public int getBook_id() {
+		return book_id;
 	}
-	public void setUpdated_at(Date updated_at) {
-		this.updated_at = updated_at;
+	public void setImg(String imgt) {
+		this.img = img;
 	}
-
+	public String getImg() {
+		return img;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
 	
 	
 }
