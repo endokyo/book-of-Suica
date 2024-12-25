85a03a4f6482d29836b590237ced0d8f020c031a
dcom <dcom@2017HP-18>
2024-12-25 09:12:58 +0900

    twicount修正

--- Suicabook/src/main/java/dao/FavoriteDao.java
+++ Suicabook/src/main/java/dao/FavoriteDao.java
@@ -91,7 +91,7 @@ public class FavoriteDao {
 				bookbean.setGenre(rs.getString("genre_name"));
 				bookbean.setAverage(rs.getDouble("book_ave"));
 				bookbean.setAvecount(rs.getInt("book_avecount"));
-				bookbean.setTwicount(rs.getInt("book_comcount"));
+				bookbean.setTwicount(rs.getInt("book_twicount"));
 				bookbean.setFavcount(rs.getInt("book_favcount"));
 				bookbean.setFavorite(true);
 				list.add(bookbean);
