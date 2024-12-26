872bd1a001809deaab59799b5c240d665c3661e0
endokyo <student07@dcom1.onmicrosoft.com>
2024-12-26 11:12:28 +0900

    commit

--- Suicabook/src/main/java/servlet/List.java
+++ Suicabook/src/main/java/servlet/List.java
@@ -38,7 +38,7 @@ public class List extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		HttpSession session = request.getSession(false);
-		UserBean user = (UserBean) session.getAttribute("user");
+		
 		CreateList createlist = new CreateList();
 		String jsp;
 //		user = new UserBean();
@@ -58,7 +58,7 @@ public class List extends HttpServlet {
 		
 		
 		//ログインされてなければログインページに飛ぶ
-		if(user == null) {
+		if(session == null) {
 			jsp = "/login.jsp";
 		}else {
 			//書籍一覧を作成
